d97bf50159cd914d98ad2193fa61978cff98cb73
ryo <riemann1618@gmail.com>
2018-09-06 12:00:53 +0900

    Use Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery3
+//= require popper
+//= bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
