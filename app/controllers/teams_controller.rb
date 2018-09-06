289947c937cddad6f47adb74889e3e2c9b4b9b53
ryo <riemann1618@gmail.com>
2018-09-06 17:12:24 +0900

    Generate TeamsController

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,10 @@
+class TeamsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'teams/show'
+  get 'teams/new'
+  get 'teams/edit'
   root 'static_pages#home'
   get :info, to: 'static_pages#info'
 end
