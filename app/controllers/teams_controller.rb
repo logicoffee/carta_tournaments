19f17ea91e703657e2c6bc346df389eb76321e6c
ryo <riemann1618@gmail.com>
2018-09-07 20:04:27 +0900

    Make up create action

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,6 +6,21 @@ class TeamsController < ApplicationController
     @team = Team.new
   end
 
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      flash[:success] = "新規かるた会登録が完了しました"
+      redirect_to team_path(@team)
+    else
+      render 'new'
+    end
+  end
+
   def edit
   end
+
+  private
+    def team_params
+      params.require(:team).permit(:name, :leader, :email, :password, :password_confirmation)
+    end
 end
