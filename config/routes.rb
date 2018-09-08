1f9955264ff051948ef4aafb583ac3c44f2b5d4d
ryo <riemann1618@gmail.com>
2018-09-08 12:25:38 +0900

    Define routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'static_pages#home'
   get :info, to: 'static_pages#info'
 
   resource :team, except: %i[new create destroy]
   get  :sign_up, to: 'teams#new'
   post :sign_up, to: 'teams#create'
+
+  get    :sign_in, to: 'sessions#new'
+  post   :sign_in, to: 'sessions#create'
+  delete :sign_out, to: 'sessions#destroy'
 end
