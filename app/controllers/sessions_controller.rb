66a4fc56988f7aa9a41a34ebc9c9b96405c6f573
ryo <riemann1618@gmail.com>
2018-09-08 13:04:59 +0900

    Make up create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    team = Team.find_by(email: params[:email])
+    if team && team.authenticate(params[:password])
+      session[:team_id] = team.id
+      redirect_to team_path
+    else
+      flash.now[:danger] = "メールアドレスまたはパスワードが正しくありません"
+      render 'new'
+    end
+  end
 end
