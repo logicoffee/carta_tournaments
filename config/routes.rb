50724f2e2ce5f8fdc1c236e2e0bf03e5726b51b3
ryo <riemann1618@gmail.com>
2018-09-09 12:16:58 +0900

    Define routing

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get    :sign_in, to: 'sessions#new'
   post   :sign_in, to: 'sessions#create'
   delete :sign_out, to: 'sessions#destroy'
+
+  get :account_activation, to: 'account_activations#activate'
 end
