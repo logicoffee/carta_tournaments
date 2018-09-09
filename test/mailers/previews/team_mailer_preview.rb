7838cf8783b76c6395113285c60cbc2f4a00e395
ryo <riemann1618@gmail.com>
2018-09-09 13:36:35 +0900

    Configuration for preview of mails

--- test/mailers/previews/team_mailer_preview.rb
+++ test/mailers/previews/team_mailer_preview.rb
@@ -3,7 +3,9 @@ class TeamMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/team_mailer/account_activation
   def account_activation
-    TeamMailer.account_activation
+    team = Team.first
+    team.activation_token = SecureRandom.urlsafe_base64
+    TeamMailer.account_activation(team)
   end
 
 end
