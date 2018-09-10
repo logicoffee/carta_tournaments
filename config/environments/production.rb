c67b955b62c5049f0c23f9ab98ee3a2566d210a3
ryo <riemann1618@gmail.com>
2018-09-10 14:24:47 +0900

    Fix a typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   host = 'carta-72nd-kyoto.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    address: 'smpt.sendgrid.net',
+    address: 'smtp.sendgrid.net',
     port: '587',
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
