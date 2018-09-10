c43d73f91e29ec2e0bc25a082983dafa248fd29b
ryo <riemann1618@gmail.com>
2018-09-10 12:49:03 +0900

    Add a line of code to ActionMailer configuratin

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'carta-72nd-kyoto.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smpt.sendgrid.net',
     port: '587',
