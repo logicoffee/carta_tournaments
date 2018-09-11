ae3ce1f36bd34b8967963e1f7c730332852f1b4e
ryo <riemann1618@gmail.com>
2018-09-11 10:44:35 +0900

    Create Player model

---
+++ db/migrate/20180911014018_create_players.rb
@@ -0,0 +1,14 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.string :rank_code
+      t.references :team, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
