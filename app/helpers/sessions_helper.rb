246fdf410c0b9fcb417585ecfdbd15e9f3d98e16
ryo <riemann1618@gmail.com>
2018-09-14 12:36:40 +0900

    Make up sign out action

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
   def current_team
     @current_team ||= Team.find_by(id: session[:team_id])
   end
+
+  def sign_out
+    session.delete(:team_id)
+    @current_team = nil
+  end
 end
