28d541bd5c3d8713d6d32b4d6b5fd55f608a0794
ryo <riemann1618@gmail.com>
2018-09-24 16:06:08 +0900

    Set validations

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,14 @@
 class Admin < ApplicationRecord
   has_secure_password
+  before_save :downcase_email
+
+  validates :email,    presence: true, length: { maximum: 255 },
+                       uniqueness: { case_sensitive: false },
+                       format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum:   8 }
+
+  private
+    def downcase_email
+      self.email.downcase!
+    end
 end
