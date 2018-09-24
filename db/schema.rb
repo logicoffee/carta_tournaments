72b8addd70a300667f0a0782a2b1a13a9afb96d5
ryo <riemann1618@gmail.com>
2018-09-24 15:19:56 +0900

    Generate Admin model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_110802) do
+ActiveRecord::Schema.define(version: 2018_09_24_060153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "players", force: :cascade do |t|
     t.string "first_name"
