8df429811e8f08c65831a788cf40ce4f925f1d46
logicoffee <riemann1618@gmail.com>
2018-10-03 13:16:57 +0900

    Add a before action

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -1,6 +1,16 @@
 class Admin::PlayersController < ApplicationController
   layout 'admin'
+  before_action :require_sign_in_as_admin
 
   def index
   end
+
+  private
+  
+    def require_sign_in_as_admin
+      unless admin_signed_in?
+        flash[:danger] = "管理者権限がありません"
+        redirect_to root_url
+      end
+    end
 end
