4921252c35661b7f20ffce368780eb7963f5815c
logicoffee <riemann1618@gmail.com>
2018-10-03 14:27:48 +0900

    Display all players

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -3,6 +3,9 @@ class Admin::PlayersController < ApplicationController
   before_action :require_sign_in_as_admin
 
   def index
+    @all_players = KyotoRank::RankData.ranks.map do |rank|
+      Player.where(rank_code: rank).order(:created_at).limit(Settings.quota[rank])
+    end
   end
 
   private
