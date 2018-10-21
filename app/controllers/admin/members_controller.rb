c73a7476af56e6cce104b4a040904b6eb64e9f6f
logicoffee <riemann1618@gmail.com>
2018-10-21 18:15:30 +0900

    Add a before action

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,6 +1,16 @@
 class Admin::MembersController < ApplicationController
   layout 'admin'
+  before_action :require_invitation_token
 
   def new
   end
+
+  private
+    def require_invitation_token
+      @quasi_admin = Admin.find_by(email: params[:email], signed_up: false)
+      unless @quasi_admin && BCrypt::Password.new(@quasi_admin.invitation_digest) == params[:id]
+        flash[:danger] = "不正なURLです"
+        redirect_to root_url
+      end
+    end
 end
