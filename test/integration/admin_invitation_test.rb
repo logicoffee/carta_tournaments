f4d5e47300fe5200ce595c7cc6c8e056da09508b
logicoffee <riemann1618@gmail.com>
2018-10-31 12:39:22 +0900

    Add an integration test for invitation

---
+++ test/integration/admin_invitation_test.rb
@@ -0,0 +1,43 @@
+require 'test_helper'
+
+class AdminInvitationTest < ActionDispatch::IntegrationTest
+  def setup
+    # 送信メールのリセット
+    ActionMailer::Base.deliveries.clear
+
+    # すでに存在する管理者でログイン
+    @admin = admins(:ryo)
+    admin_sign_in_as @admin
+  end
+
+  test "invitation flow" do
+    # メンバーの招待
+    get new_admin_invitation_path
+    inv_email = "foo@example.com"
+    assert_difference 'Invitation.count', 1 do
+      post admin_invitations_path, params: { invitation: {
+        email: inv_email
+      } }
+    end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    assert_redirected_to admin_root_url
+    assert_not flash[:success].empty?
+    token = assigns(:invitation).invitation_token
+
+    # 招待された側の処理をテストするためにログアウト
+    admin_sign_out
+
+    # 管理者登録
+    get admin_new_sign_up_path(token, email: inv_email)
+    assert_response :success
+    assert_difference 'Admin.count', 1 do
+      post admin_sign_up_path(token, email: inv_email), params: { admin: {
+        name: "招待された管理者",
+        email: inv_email,
+        password: "password",
+        password_confirmation: "password"
+      }}
+    end
+    assert admin_is_signed_in?
+  end
+end
