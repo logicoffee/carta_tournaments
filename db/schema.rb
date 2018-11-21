a684cf0a081492a6ad3be81259f481c33d8018ab
logicoffee <riemann1618@gmail.com>
2018-11-21 11:04:35 +0900

    Add extra attributes to players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_18_042432) do
+ActiveRecord::Schema.define(version: 2018_11_21_014342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2018_11_18_042432) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "deleted", default: false, null: false
+    t.jsonb "extra_attributes", null: false
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
