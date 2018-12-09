bb1ab63ca4d03c36908e51b6367cab8d5e67b784
logicoffee <riemann1618@gmail.com>
2018-12-09 14:07:24 +0900

    Add a test

--- test/integration/admin_players_lists_test.rb
+++ test/integration/admin_players_lists_test.rb
@@ -4,11 +4,15 @@ class AdminPlayersListsTest < ActionDispatch::IntegrationTest
   def setup
     @admin = admins(:ryo)
     admin_sign_in_as @admin
+    @tournament_class = tournament_classes(:advanced_class)
     @player = players(:advanced_player)
   end
 
   test "players list" do
     get admin_root_path
     assert_select 'td', text: @player.full_name
+
+    get admin_root_path(tournament_class_id: @tournament_class.id, format: :csv)
+    assert_equal 'text/csv', response.headers["Content-Type"]
   end
 end
