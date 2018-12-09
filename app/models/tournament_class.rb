89e8b58337c4b2ad526d6ed5067222ae1ac8bedf
logicoffee <riemann1618@gmail.com>
2018-12-09 14:07:35 +0900

    Add a scope

--- app/models/tournament_class.rb
+++ app/models/tournament_class.rb
@@ -1,4 +1,8 @@
 class TournamentClass < ApplicationRecord
   belongs_to :tournament
   has_and_belongs_to_many :players, join_table: :tournament_classes_players
+
+  scope :find_by_tournament_class_id, -> (id) {
+    where(id: id) unless id.nil?
+  }
 end
