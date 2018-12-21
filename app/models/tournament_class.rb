250a634b0ee08a33dad5891a6c1a63b6a907652a
logicoffee <riemann1618@gmail.com>
2018-12-21 12:46:54 +0900

    Add scopes

--- app/models/tournament_class.rb
+++ app/models/tournament_class.rb
@@ -2,7 +2,13 @@ class TournamentClass < ApplicationRecord
   belongs_to :tournament
   has_and_belongs_to_many :players, join_table: :tournament_classes_players
 
-  scope :find_by_tournament_class_id, -> (id) {
-    where(id: id) unless id.nil?
+  scope :find_and_order_by_id, -> (tournament_id:, tournament_class_id: nil) {
+    base_query = where(tournament_id: tournament_id)
+
+    if tournament_class_id.nil?
+      base_query.order(:id)
+    else
+      base_query.where(id: tournament_class_id)
+    end
   }
 end
